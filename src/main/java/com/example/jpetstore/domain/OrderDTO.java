ea5161b44156b227b896f9a0d8eb2d578c3d5244
choi <choi@localhost>
2021-05-06 16:05:17 +0900

    CoPurchasingDTO, DeliveryDTO, OrderDTO

---
+++ src/main/java/com/example/jpetstore/domain/OrderDTO.java
@@ -0,0 +1,54 @@
+package com.example.jpetstore.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class OrderDTO implements Serializable {
+
+	/* Private Fields */
+
+	private String orderId;
+	private String username;
+	private Date orderDate;
+	private String address;
+	private int totalPrice;
+
+
+
+	/* JavaBeans Properties */
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+}
