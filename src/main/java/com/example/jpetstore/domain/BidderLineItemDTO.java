29986528801c562f8c24d709af3691c4cff132a4
khk99 <khk99@LAPTOP-0PU2A6A6>
2021-05-06 16:08:17 +0900

    dto

---
+++ src/main/java/com/example/jpetstore/domain/BidderLineItemDTO.java
@@ -0,0 +1,47 @@
+package com.example.jpetstore.domain;
+
+public class BidderLineItemDTO {
+
+	private int bidderId;
+	private String address;
+	private int bidPrice;
+	
+	public BidderLineItemDTO() {}
+	
+	public BidderLineItemDTO(int bidderId) {
+		super();
+		this.bidderId = bidderId;
+	}
+	
+	public BidderLineItemDTO(int bidderId, String address, int bidPrice) {
+		super();
+		this.bidderId = bidderId;
+		this.address = address;
+		this.bidPrice = bidPrice;
+	}
+
+	public int getBidderId() {
+		return bidderId;
+	}
+	
+	public void setBidderId(int bidderId) {
+		this.bidderId = bidderId;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public int getBidPrice() {
+		return bidPrice;
+	}
+	
+	public void setBidPrice(int bidPrice) {
+		this.bidPrice = bidPrice;
+	}
+		
+}
