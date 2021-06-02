555331213ed2cd325a2537081570586bb398afab
johayeon <johayeon@localhost>
2021-06-02 17:05:13 +0900

    accountformcontroller

--- src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
+++ src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.WebUtils;
-
-import com.ssd.delivery.controller.UserSession;
 import com.ssd.delivery.domain.*;
 import com.ssd.delivery.service.AccountFormValidator;
 import com.ssd.delivery.service.DeliveryFacade;
