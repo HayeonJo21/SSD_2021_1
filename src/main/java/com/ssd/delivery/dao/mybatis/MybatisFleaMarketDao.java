e1f9b8cf3a6c10966afed181961e8e14817d3a8d
khk99 <khk99@LAPTOP-0PU2A6A6>
2021-06-02 14:20:30 +0900

    fleamarketdao

--- src/main/java/com/ssd/delivery/dao/mybatis/MybatisFleaMarketDao.java
+++ src/main/java/com/ssd/delivery/dao/mybatis/MybatisFleaMarketDao.java
@@ -8,39 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import com.ssd.delivery.dao.FleaMarketDao;
 import com.ssd.delivery.dao.mybatis.mapper.FleaMarketMapper;
+import com.ssd.delivery.dao.mybatis.mapper.ItemMapper;
 import com.ssd.delivery.domain.ItemDTO;
 
-<<<<<<< HEAD
-@Repository
-=======
-<<<<<<< HEAD
 @Repository
 public class MybatisFleaMarketDao implements FleaMarketDao{
-=======
->>>>>>> branch 'master' of https://github.com/choi1k/SSD_2021_1.git
-public class MybatisFleaMarketDao implements FleaMarketDao {
-	
-<<<<<<< HEAD
-=======
->>>>>>> refs/remotes/origin/master
-	
+
 	@Autowired
-<<<<<<< HEAD
-=======
 	private ItemMapper itemMapper;
->>>>>>> branch 'master' of https://github.com/choi1k/SSD_2021_1.git
 	@Autowired
->>>>>>> refs/remotes/origin/master
 	private FleaMarketMapper fleaMarketMapper;
 	
-<<<<<<< HEAD
-=======
-	
-<<<<<<< HEAD
-=======
-	
->>>>>>> refs/remotes/origin/master
->>>>>>> branch 'master' of https://github.com/choi1k/SSD_2021_1.git
 	public void insertFM(ItemDTO item) throws DataAccessException{
 		fleaMarketMapper.insertFM(item);
 	};
@@ -53,11 +31,7 @@ public class MybatisFleaMarketDao implements FleaMarketDao {
 		fleaMarketMapper.deleteFM(item);
 	};
 	
-<<<<<<< HEAD
-=======
-	
 
->>>>>>> refs/remotes/origin/master
 	public ItemDTO getFMById(int itemId) throws DataAccessException{
 		return fleaMarketMapper.getFMById(itemId);
 	};
