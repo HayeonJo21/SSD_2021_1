5d093bd4bdcba6f3a3e36ecb67c45d3ad6e097df
johayeon <johayeon@localhost>
2021-06-02 14:12:16 +0900

    mybatisfleamarketdao

--- src/main/java/com/ssd/delivery/dao/mybatis/MybatisFleaMarketDao.java
+++ src/main/java/com/ssd/delivery/dao/mybatis/MybatisFleaMarketDao.java
@@ -16,27 +16,14 @@ import com.ssd.delivery.domain.ItemDTO;
 import com.ssd.delivery.domain.LineItem;
 import com.ssd.delivery.domain.Order;
 
-<<<<<<< HEAD
 @Repository
 public class MybatisFleaMarketDao implements FleaMarketDao{
-=======
-public class MybatisFleaMarketDao implements FleaMarketDao {
-	
->>>>>>> refs/remotes/origin/master
-	
+
 	@Autowired
-<<<<<<< HEAD
-=======
 	private ItemMapper itemMapper;
 	@Autowired
->>>>>>> refs/remotes/origin/master
 	private FleaMarketMapper fleaMarketMapper;
-	
-	
-<<<<<<< HEAD
-=======
-	
->>>>>>> refs/remotes/origin/master
+
 	public void insertFM(ItemDTO item) throws DataAccessException{
 		fleaMarketMapper.insertFM(item);
 	};
@@ -49,11 +36,7 @@ public class MybatisFleaMarketDao implements FleaMarketDao {
 		fleaMarketMapper.deleteFM(item);
 	};
 	
-<<<<<<< HEAD
-=======
-	
 
->>>>>>> refs/remotes/origin/master
 	public ItemDTO getFMById(int itemId) throws DataAccessException{
 		return fleaMarketMapper.getFMById(itemId);
 	};
