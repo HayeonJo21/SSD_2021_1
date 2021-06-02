ec90ece1efef3d89675d623a4de079045994841d
johayeon <johayeon@localhost>
2021-06-02 18:22:10 +0900

    accountController

--- src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
+++ src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
@@ -17,60 +17,71 @@ import com.ssd.delivery.service.DeliveryFacade;
 
 //등록, 수정 둘 다 이곳에서
 @Controller
-@RequestMapping({"/user/insertAccount.do","/user/updateAccount.do"})
+@RequestMapping({"/user/insertAccount.do","/user/updateAccount.do", "user/user/newUserSubmitted.do"})
 public class AccountFormController {
-
 	@Autowired
 	private DeliveryFacade delivery;
-	
-	@Autowired
-	private AccountFormValidator validator;
-	
-	@ModelAttribute("register")
-	public AccountForm formBackingObject(HttpServletRequest request) 
-			throws Exception {
-		Account userSession = 
-			(Account) WebUtils.getSessionAttribute(request, "userSession");
-		if (userSession != null) {	// edit an existing account
-			return new AccountForm(delivery.findUser(userSession.getUsername()));
-		}
-		else {	// create a new account
-			return new AccountForm();
-		}
-	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showForm() {
 		return "register";
 	}
 	
-	@RequestMapping("user/newUserSubmitted.do")
-	public String onSubmit(HttpServletRequest request, HttpSession session,
-			@ModelAttribute("accountForm") AccountForm accountForm,
-			BindingResult result) throws Exception  {
+	@RequestMapping(method=RequestMethod.POST)
+	public String submit(AccountDTO account) {
+		account.setStatus(1);
+		delivery.insertAccount(account);
+		System.out.println("DDDD"); 
 		
-		validator.validate(accountForm, result);
-		if (result.hasErrors()) return "index";
-		
-		try {
-			if (accountForm.isNewAccount()) {
-				delivery.insertAccount(accountForm.getAccount());
-			}
-			else {
-				delivery.updateAccount(accountForm.getAccount());
-			}
-		}
-		catch (DataIntegrityViolationException ex) {
-			result.rejectValue("account.userId", "USER_ID_ALREADY_EXISTS",
-					"User ID already exists: choose a different ID.");
-			return "index"; 
-		}
-		
-		
-		AccountDTO userSession = delivery.findUser(accountForm.getAccount().getUsername());
-
-		session.setAttribute("userSession", userSession);
-		
-		return "redirect:/";
+		return "index";
 	}
+
+//	
+//	@Autowired
+//	private AccountFormValidator validator;
+//	
+//	@ModelAttribute("register")
+//	public AccountForm formBackingObject(HttpServletRequest request) 
+//			throws Exception {
+//		Account userSession = 
+//			(Account) WebUtils.getSessionAttribute(request, "userSession");
+//		if (userSession != null) {	// edit an existing account
+//			return new AccountForm(delivery.findUser(userSession.getUsername()));
+//		}
+//		else {	// create a new account
+//			return new AccountForm();
+//		}
+//	}
+//	
+
+//	
+//	@RequestMapping("user/newUserSubmitted.do")
+//	public String onSubmit(HttpServletRequest request, HttpSession session,
+//			@ModelAttribute("accountForm") AccountForm accountForm,
+//			BindingResult result) throws Exception  {
+//		
+//		validator.validate(accountForm, result);
+//		if (result.hasErrors()) return "index";
+//		
+//		try {
+//			if (accountForm.isNewAccount()) {
+//				delivery.insertAccount(accountForm.getAccount());
+//			}
+//			else {
+//				delivery.updateAccount(accountForm.getAccount());
+//			}
+//		}
+//		catch (DataIntegrityViolationException ex) {
+//			result.rejectValue("account.userId", "USER_ID_ALREADY_EXISTS",
+//					"User ID already exists: choose a different ID.");
+//			return "index"; 
+//		}
+//		
+//		
+//		AccountDTO userSession = delivery.findUser(accountForm.getAccount().getUsername());
+//
+//		session.setAttribute("userSession", userSession);
+//		
+//		return "redirect:/";
+//	}
 }
