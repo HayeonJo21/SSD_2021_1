33f4575b2e208098c481eef8b24a3981eba587a0
johayeon <johayeon@localhost>
2021-06-02 17:05:52 +0900

    AccountFormController

--- src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
+++ src/main/java/com/ssd/delivery/controller/account/AccountFormController.java
@@ -3,8 +3,6 @@ package com.ssd.delivery.controller.account;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.support.PagedListHolder;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
